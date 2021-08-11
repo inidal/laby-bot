e4c6716e01bdce572aa0558698c98498702086d1
Nidal Iguer <38238526+inidal@users.noreply.github.com>
2021-08-11 22:04:03 +0100

    Trying to implement Wiki Search in bot

--- laby/bot.py
+++ laby/bot.py
@@ -5,6 +5,7 @@ import random
 from discord.ext import commands
 from dotenv import load_dotenv
 import requests
+import wikipedia
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -19,6 +20,27 @@ async def chuck(ctx):
 @bot.command(name='roll', help='Roll the dice.')
 async def roll(ctx):
     response = random.randrange(1, 7)
+    await ctx.send(f"You rolled a {response}! 🎲")
+
+# BUGFIX NEEDED
+@bot.command(name='wiki', help='Search for information on Wikipedia.')
+async def roll(ctx):
+
+    response = []
+
+    def wiki_search(title):
+        return [title, wikipedia.summary(title)]
+
+    try:
+        title = wikipedia.search(word)[0]
+        wiki_search(title)
+    except:
+        suggest = wikipedia.suggest(word)
+        if suggest is None:
+            print("Not found.")
+        else:
+            wiki_search(suggest.capitalize())
+
     await ctx.send(f"You rolled a {response}! 🎲")
 
 bot.run(TOKEN)
--- laby/test.py
+++ laby/test.py
@@ -1,4 +1,17 @@
-import random
+import wikipedia
 
-for i in range(50):
-    print(random.randrange(1, 7))
+word = "Mrocco"
+
+def wiki_search(title):
+    print(title)
+    print(wikipedia.summary(title))
+
+try:
+    title = wikipedia.search(word)[0]
+    wiki_search(title)
+except:
+    suggest = wikipedia.suggest(word)
+    if suggest is None :
+        print("Not found.")
+    else:
+        wiki_search(suggest.capitalize())
